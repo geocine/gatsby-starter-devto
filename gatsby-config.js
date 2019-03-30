850ee1f38e14bc7af3d5d31649e0cea393552ca8
Aivan Monceller <aivan@monceller.com>
2019-03-30 21:27:15 +0800

    feat(purge): remove purge css

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-purgecss',
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    'gatsby-plugin-netlify' // make sure to keep it last in the array
   ],
 }
