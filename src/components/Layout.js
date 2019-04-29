d570384d9a9a175ed9d1ee05eaefeac40cda8f42
Aivan Monceller <aivan@monceller.com>
2019-04-29 15:40:00 +0800

    feat(header): add header

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,35 @@
 import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
 
 import './styles.scss'
 
+const Header = styled.div`
+  max-width: 825px;
+  margin: 10px auto;
+  padding: 20px;
+  width: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h3 {
+    margin: 0;
+    color: #0a0a0a;
+  }
+`
 
 const TemplateWrapper = ({ children }) => (
   <Fragment>
     <Helmet title="Home | Gatsby + Dev.to" />
-    <main>{children}</main>
+    <main>
+      <Header>
+        <h3>
+          <Link to="/">gatsby-starter-devto</Link>
+        </h3>
+      </Header>
+      {children}
+    </main>
   </Fragment>
 )
 
